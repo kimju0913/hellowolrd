cafed49e6c05c58b67b74694be2100a5e59a76bf
Georges JK Kim <kimju0913@naver.com>
2020-07-01 14:46:53 +0900

    Update

--- HelloWorld/src/main/java/com/spring/helloworld/interceptor/HomeSessionInterceptor.java
+++ HelloWorld/src/main/java/com/spring/helloworld/interceptor/HomeSessionInterceptor.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+// homeId 세션 생성 인터셉터
 public class HomeSessionInterceptor extends HandlerInterceptorAdapter{
 	Logger logger = LoggerFactory.getLogger(HomeSessionInterceptor.class);
 	
@@ -16,60 +17,20 @@ public class HomeSessionInterceptor extends HandlerInterceptorAdapter{
 			throws Exception {
 		HttpSession session =request.getSession();
 		
-		logger.info(request.getRequestURL().toString());
 		String homeId = (String)session.getAttribute("homeId");
 		String homeParameter = (String) request.getParameter("homeId");
-		logger.info( "=============홈파라미터 : " + homeParameter + "=========홈세션 :"+ homeId);
-		//  Ķ     jsp    value      home id                         
-		
 		String loginId = (String)request.getSession().getAttribute("loginId");
 		
+		// 로그인 세션 없을 시 로그인 화면으로 리다이렉트
 		if(loginId == null ) {
 			response.sendRedirect("/helloworld/user/login");
-			logger.info("    ̷ ? ");
 			return false;
 		}
 		
-		if(homeParameter != null   ) {
-			session.setAttribute("homeId", homeParameter);
-			logger.info("homeParameter"+(String) session.getAttribute("homeId"));
-			
-			if(homeId != null) {
-			if(!homeId.equals(homeParameter) && homeParameter != null) {
-			logger.info("   ǰ   Ķ   Ͱ   ٸ    ");
-				request.getSession().setAttribute("homeId", homeParameter);
-			}
-			}
-			
-		}else if( homeId ==  null ) {
-			session.setAttribute("homeId", loginId);
-			logger.info("homeId"+(String) session.getAttribute("homeId"));
-			
-			
-		}
-
+		// homeParameter 값을 받아 homeId Session 생성
+		session.setAttribute("homeId", homeParameter);
 		
-//		if(homeSession == null && homeParameter == null) {
-//			
-//			response.sendRedirect("/helloworld/user/login");
-//			logger.info("    ̷ ? ");
-//			
-//		}
-//		
-//		
-//		if(homeSession != null ) {
-//			request.getSession().setAttribute("homeId", homeSession);
-//			logger.info("   ͼ         ׽ Ʈ" + request.getSession().getAttribute("homeId"));
-//			
-//			if(!homeSession.equals(homeParameter) && homeParameter != null) {
-//				logger.info("   ǰ   Ķ   Ͱ   ٸ    ");
-//				request.getSession().setAttribute("homeId", homeParameter);
-//			}
-//			
-//		} else if(homeSession == null) {
-//			logger.info("   ǰ   Ķ   Ͱ   ٸ    ");
-//			request.getSession().setAttribute("homeId", homeParameter);
-//		}
+		
 		
 		
 		return true;
